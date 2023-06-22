81d8dfcfd3ff1293a0ea279ea7c9e548df509a1d
chrisrbd <christophe.reybaud@efrei.net>
2023-06-22 16:00:30 +0200

    backup.py  update (terminé)

--- ClientApp/DataBackupSystem/backup.py
+++ ClientApp/DataBackupSystem/backup.py
@@ -6,80 +6,74 @@ import datetime
 
 def full_backup(source_dir, backup_dir):
     timestamp = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-    backup_dir = os.path.join(backup_dir, f"full_backup{timestamp}")
+    backup_dir = os.path.join(backup_dir, f"backup{timestamp}")
     shutil.copytree(source_dir, backup_dir)
     print(f"Sauvegarde complète effectuée : {backup_dir}")
 
 
-def get_last_full_backup(backup_dir):
-    full_backups = [f for f in os.listdir(backup_dir) if f.startswith("full_backup")]
-    if full_backups:
-        full_backups.sort(reverse=True)
-        return os.path.join(backup_dir, full_backups[0])
-    else:
-        return None
-
-
-def get_last_partial_backup(backup_dir):
-    partial_backups = [f for f in os.listdir(backup_dir) if f.startswith("partial_backup")]
-    if partial_backups:
-        partial_backups.sort(reverse=True)
-        return os.path.join(backup_dir, partial_backups[0])
+def get_last_backup(backup_dir):
+    last_backups = [f for f in os.listdir(backup_dir) if f.startswith("backup")]
+    if last_backups:
+        last_backups.sort(reverse=True)
+        return os.path.join(backup_dir, last_backups[0])
     else:
         return None
 
 
 def partial_backup(source_dir, backup_dir):
-    timestamp = datetime.datetime.now().strftime("%d%m%Y%H%M%S")
-    last_full_backup = get_last_full_backup(backup_dir)
-    last_partial_backup = get_last_partial_backup(backup_dir)
+    last_backup = get_last_backup(backup_dir)
 
-    if last_full_backup is None:
-        print("Aucune sauvegarde complète précédente trouvée.")
+    if last_backup is None:
+        print("Erreur: Aucune sauvegarde n'a été trouvé ! Veuillez démarrer une sauvegarde complète de la machine ou "
+              "bien contactez votre administrateur.")
         return
 
-    partial_backup_dir = os.path.join(backup_dir, f"partial_backup{timestamp}")
-    os.makedirs(partial_backup_dir)
-
-    copied_files = set()
-
-    if last_partial_backup is not None:
-        for root, _, files in os.walk(last_partial_backup):
-            for file in files:
-                copied_files.add(os.path.relpath(os.path.join(root, file), last_partial_backup))
-
     for root, _, files in os.walk(source_dir):
-        relative_path = os.path.relpath(root, source_dir)
-        destination_dir = os.path.join(partial_backup_dir, relative_path)
-        os.makedirs(destination_dir, exist_ok=True)
-
         for file in files:
             source_path = os.path.join(root, file)
-            destination_path = os.path.join(destination_dir, file)
+            relative_path = os.path.relpath(source_path, source_dir)
+            backup_path = os.path.join(last_backup, relative_path)
 
-            source_modified_time = os.path.getmtime(source_path)
-            last_full_backup_file = os.path.join(last_full_backup, relative_path, file)
+            if os.path.exists(backup_path):
+                # Compare modification timestamps to check if the file has been modified
+                if os.path.getmtime(source_path) > os.path.getmtime(backup_path):
+                    # Copy the modified file to the new backup directory
+                    new_backup_path = os.path.join(last_backup, relative_path)
+                    os.makedirs(os.path.dirname(new_backup_path), exist_ok=True)
+                    shutil.copy2(source_path, new_backup_path)
+            else:
+                # Copy new file to the new backup directory
+                new_backup_path = os.path.join(last_backup, relative_path)
+                os.makedirs(os.path.dirname(new_backup_path), exist_ok=True)
+                shutil.copy2(source_path, new_backup_path)
 
-            if not os.path.exists(last_full_backup_file) or os.path.getmtime(last_full_backup_file) < source_modified_time:
-                shutil.copy2(source_path, destination_path)
-                copied_files.add(os.path.relpath(source_path, source_dir))
-
-    for file in copied_files:
-        source_path = os.path.join(source_dir, file)
-        destination_path = os.path.join(partial_backup_dir, file)
-
-        if not os.path.exists(destination_path):
-            shutil.copy2(source_path, destination_path)
-
-    print(f"Sauvegarde partielle effectuée : {partial_backup_dir}")
+    print(f"Sauvegarde partielle effectuée : {last_backup}")
 
 
-# Exemple d'utilisation
-source_dir = "source_dir"
-backup_dir = "backup_dir"
 
-# Sauvegarde complète
-# full_backup(source_dir, backup_dir)
 
-# Sauvegarde partielle
-partial_backup(source_dir, backup_dir)
+while True:
+    print("\n==============================================")
+    print("Bienvenue dans votre système de sauvegarde :)")
+    print("==============================================")
+    print("1) Sauvegarde complète")
+    print("2) Sauvegarde incrémentale")
+    print("3) Quitter")
+    choix = input('\nQue choisissez-vous de faire ?\n')
+    source_dir = "source_dir"
+    backup_dir = "backup_dir"
+    if choix.strip() == "1":
+        # Sauvegarde complète
+        full_backup(source_dir, backup_dir)
+        tempo = input('Appuyez sur une touche pour continuer...')
+    elif choix.strip() == "2":
+        # Sauvegarde incrémentale
+        partial_backup(source_dir, backup_dir)
+
+        tempo = input('Appuyez sur une touche pour continuer...')
+    elif choix.strip() == "3":
+        break
+    else:
+        print("Votre choix n'est pas valide. Tapez 1), 2) ou 3)\n")
+        tempo = input('Appuyez sur une touche pour recommencer...')
+        continue
