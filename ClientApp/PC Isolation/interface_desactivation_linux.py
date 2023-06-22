189fa30593fe9c4fa49c5b165dab3dafd68bc94a
yaya47520 <120166288+yaya47520@users.noreply.github.com>
2023-06-22 15:30:15 +0200

    désativation interface tous corrigé

---
+++ ClientApp/PC Isolation/interface_desactivation_linux.py
@@ -0,0 +1,14 @@
+﻿import subprocess
+
+def disable_interfaces_linux(interface):
+    cmd = f"ip link set {interface} down"
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+        print(f"L'interface \"{interface}\" a été désactivée.")
+    except subprocess.CalledProcessError:
+        print(f"Erreur lors de la désactivation de l'interface \"{interface}\".")
+
+# Appeler la fonction pour désactiver la connexion Wi-Fi
+# disable_interfaces("Wi-Fi")
+# enp0s3
+# enp0s8
