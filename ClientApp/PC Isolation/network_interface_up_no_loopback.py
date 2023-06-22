b51464163533111f3fc6b17a8b7dc972d7664da9
yaya47520 <120166288+yaya47520@users.noreply.github.com>
2023-06-22 15:29:46 +0200

    désativation interface tous corrigé

--- ClientApp/PC Isolation/network_interface_up_no_loopback.py
+++ ClientApp/PC Isolation/network_interface_up_no_loopback.py
@@ -1,9 +1,11 @@
 import psutil
 import platform
-system = platform.system()
 from test_loopback import is_loopback
 from test_loopback_mac import is_loopback_mac
-from interface_deactivation import disable_interfaces
+from test_loopback_linux import is_loopback_linux
+from interface_desactivation import disable_interfaces
+from interface_desactivation_linux import disable_interfaces_linux
+from interface_desactivation_mac import disable_interfaces_mac
 
 def get_network_interfaces():
     interfaces = []
@@ -17,14 +19,24 @@ network_interfaces = get_network_interfaces()
 
 interfaces_to_disabled = []
 # Affichage des noms des interfaces réseau
-
+system = platform.system()
 if system == "Windows":
     for interface in network_interfaces:
         if not is_loopback(interface):
             interfaces_to_disabled.append(interface)
+    for interface in interfaces_to_disabled:
+        disable_interfaces(interface)
 if system == "Darwin":
     for interface in network_interfaces:
         if not is_loopback_mac(interface):
             interfaces_to_disabled.append(interface)
+    for interface in interfaces_to_disabled:
+        disable_interfaces_mac(interface)
+if platform.system() == 'Linux':
+    for interface in network_interfaces:
+        if not is_loopback_linux(interface):
+            interfaces_to_disabled.append(interface)
+    for interface in interfaces_to_disabled:
+        disable_interfaces_linux(interface)
 for interface in interfaces_to_disabled:
     disable_interfaces(interface)
