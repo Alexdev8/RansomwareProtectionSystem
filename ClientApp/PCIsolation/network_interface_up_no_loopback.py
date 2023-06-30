22a8b07b39409466c63d75a28c10d3e1e9e0277b
yaya47520 <120166288+yaya47520@users.noreply.github.com>
2023-06-30 09:48:21 +0200

    desactivation interface fonction

--- ClientApp/PCIsolation/network_interface_up_no_loopback.py
+++ ClientApp/PCIsolation/network_interface_up_no_loopback.py
@@ -13,31 +13,33 @@ def get_network_interfaces():
         if stats.isup:
             interfaces.append(interface)
     return interfaces
+def desactivation_interfaces():
+    # Appel de la fonction pour obtenir les interfaces réseau
+    network_interfaces = get_network_interfaces()
 
-# Appel de la fonction pour obtenir les interfaces réseau
-network_interfaces = get_network_interfaces()
+    interfaces_to_disabled = []
 
-interfaces_to_disabled = []
-# Affichage des noms des interfaces réseau
-system = platform.system()
-if system == "Windows":
-    for interface in network_interfaces:
-        if not is_loopback(interface):
-            interfaces_to_disabled.append(interface)
-            print(interface)
-    for interface in interfaces_to_disabled:
-        disable_interfaces(interface)
-if system == "Darwin":
-    for interface in network_interfaces:
-        if not is_loopback_mac(interface):
-            interfaces_to_disabled.append(interface)
-            print(interface)
-    for interface in interfaces_to_disabled:
-        disable_interfaces_mac(interface)
-if system == 'Linux':
-    for interface in network_interfaces:
-        if not is_loopback_linux(interface):
-            interfaces_to_disabled.append(interface)
-            print(interface)
-    for interface in interfaces_to_disabled:
-        disable_interfaces_linux(interface)
+    # Affichage des noms des interfaces réseau
+    system = platform.system()
+    if system == "Windows":
+        for interface in network_interfaces:
+            if not is_loopback(interface):
+                interfaces_to_disabled.append(interface)
+                print(interface)
+        for interface in interfaces_to_disabled:
+            disable_interfaces(interface)
+    if system == "Darwin":
+        for interface in network_interfaces:
+            if not is_loopback_mac(interface):
+                interfaces_to_disabled.append(interface)
+                print(interface)
+        for interface in interfaces_to_disabled:
+            disable_interfaces_mac(interface)
+    if system == 'Linux':
+        for interface in network_interfaces:
+            if not is_loopback_linux(interface):
+                interfaces_to_disabled.append(interface)
+                print(interface)
+        for interface in interfaces_to_disabled:
+            disable_interfaces_linux(interface)
+    return interfaces_to_disabled
